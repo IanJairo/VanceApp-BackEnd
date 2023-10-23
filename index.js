258039f9df3522c3dbb3e9eb1cfaa5bb48e1d5c6
Ian Jairo <41593870+IanJairo@users.noreply.github.com>
2023-10-23 11:51:40 -0300

    Feature: Template Email Message + ShareNote route

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const morgan = require('morgan');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 
+const ejs = require('ejs');
+const fs = require('fs');
 const db = require('./db');
 const app = express();
-const transporter = require('./nodemailer');
+const transporter = require('./helpers/nodemailer');
 app.use(express.json());
 app.use(morgan('dev'))
 
@@ -126,6 +128,64 @@ const Note = {
             response.status = 500;
             res.status(response.status).json(response);
         }
+    },
+
+    async shareNote(req, res) {
+        // Rota para compartilhar uma nota com outro usuário
+        const { noteId, email, canEdit } = req.body;
+        let response = { error: '' };
+
+        console.log(req.body);
+        try {
+            // Verifica se a nota existe no banco de dados
+            const note = await db.query('SELECT * FROM note WHERE id = $1', [noteId]);
+
+            console.log(note.rows.length)
+            if (note.rows.length === 0) {
+                response = { error: 'Note not found', status: 204, message: "Nota inexistente." };
+                return res.status(response.status).json(response);
+            }
+
+            // Verifica se o usuário atual é o autor da nota
+            if (note.rows[0].user_id !== req.body.user.id) {
+                response = { error: 'Note not found', status: 204, message: "A nota já pertence ao usuário." };
+                return res.status(response.status).json(response);
+            }
+
+            // Verifica se o email do destinatário existe no banco de dados
+            const recipient = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+            console.log(recipient.rows)
+            if (recipient.rows.length === 0) {
+                response = { error: 'Recipient not found', status: 204, message: "Destinatario não encontrado." };
+                console.log("entrou?", response);
+                return res.status(response.status).json(response);
+            }
+
+            console.log("recipient.rows[0].id", recipient.rows[0].id)
+
+            // Verifica se a nota já foi compartilhada com o destinatário
+            const isShared = await db.query('SELECT * FROM user_note WHERE note_id = $1 AND user_id = $2', [noteId, recipient.rows[0].id]);
+
+            console.log("isShared.rows", isShared)
+
+            if (isShared.rows.length > 0) {
+                console.error("entrou?")
+                response = { error: 'Note already shared with recipient', status: 204, message: "Nota já pertence ao destinatário." };
+                return res.status(response.status).json(response);
+            }
+
+            console.log("passou")
+
+            // Compartilha a nota com o destinatário
+            await db.query('INSERT INTO user_note (note_id, user_id, can_edit) VALUES ($1, $2, $3)', [noteId, recipient.rows[0].id, canEdit]);
+
+            response = { status: 200, message: "Nota compartilhada." };
+            res.status(response.status).json(response);
+        } catch (err) {
+            response.error = err.message;
+            response.status = 500;
+            res.status(response.status).json(response);
+        }
     }
 };
 
@@ -138,7 +198,7 @@ const User = {
             if (result.rows.length > 0) {
                 response.error = 'Email already exists';
                 response.status = 409;
-                return res.status(response.error).json(response);
+                return res.status(response.status).json(response);
             }
             const hash = await bcrypt.hash(password, 10);
             const insertResult = await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id', [name, email, hash]);
@@ -146,7 +206,7 @@ const User = {
             response.status = 201;
             response.data = { id: insertResult.rows[0].id };
 
-            res.status(response.status).json({ response });
+            res.status(response.status).json(response);
         } catch (err) {
             response.error = err.message;
             response.status = 500;
@@ -171,11 +231,22 @@ const User = {
             // Salva o PIN no banco de dados
             await db.query('UPDATE users SET pin = $1 WHERE email = $2', [pin, email]);
 
+
+            // Lê o conteúdo do arquivo HTML
+            const messageHtml = fs.readFileSync('./helpers/message.html', 'utf8');
+
+
+
+            // Renderiza o HTML com o PIN preenchido
+            const renderedHtml = ejs.render(messageHtml, { "pin": pin, "user": user.rows[0].name });
+
+
             const mailOptions = {
                 from: 'vance.app@hotmail.com',
                 to: email,
-                subject: '[VANCE] Recuperação de senha',
-                text: `Seu PIN de recuperação de senha é ${pin}`
+                subject: '[VANCE] PIN de recuperação de Senha',
+                text: `Seu PIN de recuperação de senha é ${pin}`,
+                html: renderedHtml
             };
 
             // Envia o PIN por e-mail
@@ -226,22 +297,22 @@ const User = {
         let response = { error: '' };
 
         try {
-          // Verifica se o email existe no banco de dados
-          const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
-          if (user.rows.length === 0) {
-            return res.status(404).json({ error: 'User not found' });
-          }
-      
-          // Gera um hash da nova senha
-          const salt = await bcrypt.genSalt(10);
-          const hash = await bcrypt.hash(newPassword, salt);
-      
-          // Atualiza a senha do usuário no banco de dados
-          await db.query('UPDATE users SET password = $1 WHERE email = $2', [hash, email]);
-      
-          response.status = 200;
-          response.message = 'Password changed';
-          res.status(response.status).json(response);
+            // Verifica se o email existe no banco de dados
+            const user = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+            if (user.rows.length === 0) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            // Gera um hash da nova senha
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(newPassword, salt);
+
+            // Atualiza a senha do usuário no banco de dados
+            await db.query('UPDATE users SET password = $1 WHERE email = $2', [hash, email]);
+
+            response.status = 200;
+            response.message = 'Password changed';
+            res.status(response.status).json(response);
         } catch (err) {
             response.error = err.message;
             response.status = 500;
@@ -314,6 +385,8 @@ app.listen(3000, () => {
     console.log('Server on port 3000')
 });
 
+// Share a note with another user
+app.post('/api/notes/share', Note.shareNote);
 
 app.post('/api/signup', User.signup);
 
